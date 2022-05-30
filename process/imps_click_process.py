b9f33b8684d511a9d10e9584bbb4b01c6b0e4031
bidgear <nam.nn@bidgear.com>
2022-05-30 10:53:19 +0700

    edit

--- process/imps_click_process.py
+++ process/imps_click_process.py
@@ -1,5 +1,8 @@
 from utils.utils import do_agg
+import time
 
 
 def process(folder_path, path, today, list_processing_hour, server_host, list_config, header):
-    do_agg(folder_path, path, today, list_processing_hour, server_host, list_config, header)
+    while True:
+        do_agg(folder_path, path, today, list_processing_hour, server_host, list_config, header)
+        time.sleep(180)
