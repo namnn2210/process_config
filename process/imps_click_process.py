ed5fe7f7339aba59daddefa00d79d5bfdf47a219
bidgear <nam.nn@bidgear.com>
2022-06-01 09:47:55 +0700

    edit

--- process/imps_click_process.py
+++ process/imps_click_process.py
@@ -1,5 +1,8 @@
 from utils.utils import start
 
 
-def process(folder_path, path, list_processing_hour, server_host, list_config, header, is_crontab):
-    start(folder_path, path, list_processing_hour, server_host, list_config, header, is_crontab)
+def process(folder_path, path, list_processing_hour, server_host, list_config, header, is_last_hour, is_day,
+            is_yesterday,
+            is_crontab):
+    start(folder_path, path, list_processing_hour, server_host, list_config, header, is_last_hour, is_day, is_yesterday,
+          is_crontab)
